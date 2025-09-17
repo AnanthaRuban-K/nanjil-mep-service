ede2fc91b2883a8e013ecd9a6ff9c2616ce4fbda
AnanthaRuban-K <website@sbrostech.com>
2025-09-17 11:39:31 +0530

    html remove fromsrc/layout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    
       <body className={inter.className}>
         <ClientProviders>{children}</ClientProviders>
       </body>
-    </html>
+  
   );
 }
