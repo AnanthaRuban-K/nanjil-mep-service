0c2759822f7ede7da78824b34129110f06c2cb5e
AnanthaRuban-K <website@sbrostech.com>
2025-09-17 10:11:38 +0530

    url update in api client

--- apps/frontend/src/lib/api/client.ts
+++ apps/frontend/src/lib/api/client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3101',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://api.nanjilmepservice.com', 
   headers: {
     'Content-Type': 'application/json',
   },
